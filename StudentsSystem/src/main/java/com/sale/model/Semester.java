03cf0bc9a52d22e981c47a0d049282e72c7a1f6f
Swapnil Yeutkar <106110395+Swapnil28Y@users.noreply.github.com>
2023-02-23 16:51:23 +0530

    done

---
+++ StudentsSystem/src/main/java/com/sale/model/Semester.java
@@ -0,0 +1,20 @@
+package com.sale.model;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Semester {
+	private int semId;
+	private int English = 0;
+	private int Maths = 0;
+	private int Science = 0;
+}
---
+++ StudentsSystem/src/main/java/com/sale/model/Student.java
@@ -0,0 +1,30 @@
+package com.sale.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@Document(indexName = "student-index" )
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Student {
+	@Id
+	private String id;
+	@Field(type = FieldType.Text , name = "name")
+	private String name;
+	@Field(type = FieldType.Auto, name = "semesters")
+	private List<Semester> semesters;
+}
