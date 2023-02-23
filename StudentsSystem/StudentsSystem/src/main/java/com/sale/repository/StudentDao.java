b30c83872e61def83905b7e7799ffebdfd46fe66
Swapnil Yeutkar <106110395+Swapnil28Y@users.noreply.github.com>
2023-02-23 22:15:40 +0530

    Delete StudentsSystem/StudentsSystem directory

--- StudentsSystem/StudentsSystem/src/main/java/com/sale/repository/StudentDao.java
+++
@@ -1,8 +0,0 @@
-package com.sale.repository;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import com.sale.model.Student;
-
-public interface StudentDao extends ElasticsearchRepository<Student, String> {
-
-}
